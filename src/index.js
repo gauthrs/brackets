af14a3c71db65ae23b7721ef2e5f2c0ee0fdd33f
Gauthier <gotyeme@gmail.com>
2020-10-20 22:09:50 +0300

    feat: add task execution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+  let object = Object.fromEntries(bracketsConfig);
+
+  for(const i of str.split('')) {
+      if (array[0] === i) {
+          array.shift();
+      } else if(object[i]) {
+          array.unshift(object[i]);
+      } else {
+          return false;
+      }
+  }
+
+  return array.length === 0;
 }
